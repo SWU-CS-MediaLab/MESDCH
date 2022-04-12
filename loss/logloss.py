5c14c9b271868569ff7008e5797adaa54c1d1bfc
SWU-CS-MediaLab <xinzhiwang1997@gmail.com>
2022-04-12 16:44:39 +0800

    Delete loss directory

--- loss/logloss.py
+++
@@ -1,8 +0,0 @@
-import torch
-def logloss(labels_batchsize, labels_train, hashrepresentations_batchsize, hashrepresentations__train):
-    batch_size = labels_batchsize.shape[0]
-    num_train = labels_train.shape[0]
-    S = (labels_batchsize.matmul(labels_train.transpose(0, 1)) > 0).type(torch.cuda.FloatTensor)
-    theta = 1.0 / 2 * torch.matmul(hashrepresentations_batchsize, hashrepresentations__train.t())
-    logloss = -torch.sum(S * theta - torch.log(1.0 + torch.exp(theta))) / (batch_size * num_train)
-    return logloss
